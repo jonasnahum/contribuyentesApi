f797184bb1d504f5e6be307263e7a87e9261541f
jonasnahum <jonasnahum@gmail.com>
2015-03-05 00:15:08 +0000

    fixed typo

--- server/routes/contribuyentes.js
+++ server/routes/contribuyentes.js
@@ -1,7 +1,7 @@
 "use strict";
 
 exports.init = function (express, db) {
-    var Database = require("./../../communications/database.js").init();
+    var Database = require("./../../communication/database.js").init();
     var Model = require("./../models/contribuyentesModel.js").init();
     
     express = express || require('express');
